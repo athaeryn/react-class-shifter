7f45e38be7b66a0fa201f14e57adc346d90d6a70
Mike Anderson <hello@mrmikea.com>
2015-02-20 00:26:41 -0600

    Implement basic class shifting

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ react-class-shifter
 ===================
 
 A React component that facilitates animations by shifting classes around.
+
+
+```jsx
+React.render((
+  <ClassShifter>
+    <div key="hey">hey there</div>
+    <div key="hi">hi there</div>
+    <div key="ho">ho there</div>
+  </ClassShifter>
+), node);
+```
+
+
+## Todo:
+
+- Pause on hover
+- .prev and .next classes
+- .before and .after?
--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-var React = require("react");
+var React = require("react"),
+    addons = require("react/addons").addons,
+    cloneWithProps = addons.cloneWithProps,
+    classSet = addons.classSet;
 
 module.exports = React.createClass({
+  getDefaultProps() {
+    return {
+      speed: 1000,
+      className: "class-shifter"
+    };
+  },
+
+  getInitialState() {
+    return {
+      interval: null,
+      count: this.props.children.length,
+      index: 0
+    };
+  },
+
+  componentDidMount() {
+    this.interval = setInterval(this.advance, this.props.speed);
+  },
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  },
+
+  advance() {
+    var nextIndex = this.state.index + 1;
+        if (nextIndex >= this.state.count) {
+          nextIndex = 0;
+        }
+    this.setState({index: nextIndex});
+  },
+
   render() {
-    let greeting = `Hello world, it's ${(new Date).toLocaleDateString()}`;
+    var children = this.props.children.map((child, index) => {
+      var props = {
+        // Maintain child key
+        key: child.key,
+        className: classSet({active: this.state.index == index})
+      };
+      return cloneWithProps(child, props);
+    });
+
     return (
-      <h1>{greeting}</h1>
+      <div className={this.props.className}>{children}</div>
     );
   }
 });
